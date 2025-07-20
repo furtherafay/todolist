a232b0558490fb009fff4ef10d33b84712b08ff9
Rafay Siddiqui <122159345+furtherafay@users.noreply.github.com>
2025-07-21 02:00:58 +0500

    feat: added responsiveness to calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,23 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Calendar = () => {
-  const [days, setDays] = useState([
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-    "Sun",
-  ]);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const today = new Date();
+
+  const [month, setMonth] = useState(today.getMonth());
+  const [date, setDate] = useState(today.getDate());
+  const [day, setDay] = useState(today.getDay());
+  const [selectedIndex, setSelectedIndex] = useState();
+
+  const getDate = () => {
+    return `${days[day]}, ${months[month]} ${date}`;
+  };
+
+  const handleChangeDate = (index) => {
+    setSelectedIndex(index);
+    setDay(index);
+
+    const clickDiff = index - day;
+    setDate(date + clickDiff);
+  };
 
   return (
     <>
       <div className="flex flex-col ml-2 mr-2 mt-2 w-[90%] h-26 border border-black rounded-md">
-        <h1 className="text-2xl font-mono font-bold ml-2">Today, July 21st</h1>
+        <h1 className="text-2xl font-mono font-bold ml-2">{getDate()}</h1>
         <div className="flex flex-row justify-around font-mono ml-2 mt-6">
-          {days.map((d) => (
-            <div className="flex justify-center items-center w-[10%] h-full border border-black rounded-md">{d}</div>
+          {days.map((d, index) => (
+            <div
+              key={d}
+              onClick={() => handleChangeDate(index)}
+              className={`cursor-pointer flex justify-center items-center w-[10%] h-full border border-black rounded-md
+              ${selectedIndex === index ? "bg-muted-foreground text-white" : "hover:bg-muted-foreground"}
+              `}
+            >
+              {d}
+            </div>
           ))}
         </div>
       </div>
